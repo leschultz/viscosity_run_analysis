f720b0e41f4441bd46e20ee3d1a258bc697eb369
Lane Enrique Schultz <lsschultz@wisc.edu>
2019-09-23 09:17:57 -0500

    Made algorithm to aquire tg

---
+++ fragility.py
@@ -0,0 +1,251 @@
+from matplotlib import pyplot as pl
+
+from functions import finder, data_parse
+from math import e
+
+from scipy.constants import physical_constants
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
+
+import pandas as pd
+import scipy as sc
+import numpy as np
+import argparse
+import ast
+import os
+import re
+
+
+parser = argparse.ArgumentParser(
+                                 description='Arguments for gathering Tg.'
+                                 )
+
+parser.add_argument(
+                    '-d',
+                    action='store',
+                    type=str,
+                    help='The location of data.'
+                    )
+
+parser.add_argument(
+                    '-n',
+                    action='store',
+                    type=str,
+                    help='The name of the thermodynamic data files.'
+                    )
+
+parser.add_argument(
+                    '-v',
+                    action='store',
+                    type=str,
+                    help='The name of the viscosity data files.'
+                    )
+
+parser.add_argument(
+                    '-t',
+                    action='store',
+                    type=str,
+                    help='The file containing the hold temperature.'
+                    )
+
+parser.add_argument(
+                    '-c',
+                    action='store',
+                    type=str,
+                    help='The viscosity choice for T*.'
+                    )
+
+args = parser.parse_args()
+
+
+def find_nearest(array, value):
+    '''
+    Find the nearest point.
+
+    inputs:
+        array = The data array.
+        value = The point in question.
+
+    outputs:
+        idx = The index of the nearest value.
+    '''
+
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+
+    return idx
+
+
+def fragility_plots(
+                    fig,
+                    ax,
+                    x,
+                    y,
+                    xfit,
+                    yfit,
+                    visc_cut,
+                    label,
+                    ):
+    '''
+    The plotting code for Tg.
+
+    inputs:
+        fig = The figure object.
+        ax = The axes object.
+
+        x = The temperature data.
+        y = The viscosity data.
+        xfit = x data used for interpolation.
+        yfit = The interpolation between data points.
+        visc_cut = The cutoff viscosity.
+        label = The label for the data.
+
+    outputs:
+        NA
+    '''
+
+    line = ax.plot(
+                   x,
+                   y,
+                   marker='.',
+                   linestyle='none',
+                   )
+
+    color = line[0].get_color()
+
+    ax.plot(
+            xfit,
+            yfit,
+            color=color,
+            label=label
+            )
+
+    ax.grid()
+    ax.legend()
+
+    ax.set_xlabel(r'$T_{g}/Temperature$ $[K^{-1}]$')
+    ax.set_ylabel(r'Viscosity $[Pa \cdot s]$')
+    ax.set_yscale('log')
+
+    fig.tight_layout()
+
+
+def run_iterator(path, filename, viscname, tempfile, visc_cut):
+    '''
+    Iterate through each run and gather data.
+
+    inputs:
+        path = The path where run data is.
+        filename = The name of the file containing thermodynamic data.
+        viscname = The name of the file containing viscosity data.
+        tempfile = The name of the file containing the hold temperature.
+        visc_cut = The viscosity choice for T*.
+
+    outputs:
+        df = The data from runs.
+    '''
+
+    # Gather all applicable paths
+    print(filename, path)
+    paths = finder(filename, path)
+
+    k = physical_constants['Boltzmann constant in eV/K'][0]
+
+    # Gather all data and make one dataframe
+    df = []
+    for item in paths:
+        job = os.path.commonprefix([item, path])
+        job = item.strip(job)
+
+        cols, data = data_parse(os.path.join(item, filename))
+        data = pd.DataFrame(data, columns=cols)
+
+        cols, visc = data_parse(os.path.join(item, viscname))
+        visc = pd.DataFrame(visc, columns=cols)
+
+        data = data.merge(visc)
+
+        # Set beggining steps to zero
+        data['TimeStep'] -= min(data['TimeStep'])
+        data['run'] = job
+
+        # Hold temperature
+        temp = float(
+                     np.loadtxt(os.path.join(item, tempfile))
+                     )  # Hold temperature
+
+        data['hold_temp'] = temp
+        df.append(data)
+
+    df = pd.concat(df)
+    df['job'] = df['run'].apply(lambda x: x.split('/run/')[0])
+
+    # The number of atoms from naming convention
+    df['atoms'] = df['job'].apply(lambda x: re.split('(\d+)', x))
+    df['atoms'] = df['atoms'].apply(lambda x: [i for i in x if i.isnumeric()])
+    df['atoms'] = df['atoms'].apply(lambda x: [int(i) for i in x])
+    df['atoms'] = df['atoms'].apply(lambda x: sum(x))
+
+    # Calculate E-3kT
+    df['etot'] = df['pe']+df['ke']
+    df['etot/atoms'] = df['etot']/df['atoms']
+    df['etot/atoms-3kT'] = df['etot/atoms']-3.0*k*df['hold_temp']
+
+    # Group data by run
+    groups = df.groupby(['job', 'hold_temp'])
+    mean = groups.mean().add_suffix('_mean').reset_index()
+    groups = mean.groupby(['job'])
+
+    # Determine Tg from minimum value (only works for super small systems)
+    tg = mean.loc[groups['etot/atoms-3kT_mean'].idxmin()][['job', 'temp_mean']]
+    mean = mean.merge(tg, on='job', how='outer', suffixes=('', '_Tg'))
+    mean.rename(columns={'temp_mean_Tg':'Tg'}, inplace=True)
+    groups = mean.groupby(['job'])
+
+    fig, ax = pl.subplots()
+
+    data = {'job': [], 'tg/tstar': [], 'visc':[]}
+    for i, j in groups:
+        data['job'].append(i)
+
+        x = j['temp_mean'].values
+        y = j['visc_mean'].values
+
+        tg = np.unique(j['Tg'].values)[0]  # Tg based on minimum of curve
+        numerator = tg
+
+        # Cut viscosities below Tg
+        indexes = (x > tg)
+        x = x[indexes]
+        y = y[indexes]
+
+        xfit = np.linspace(min(x), max(x), 1000)
+        yfit = interp1d(x, y)(xfit)
+
+        idx = find_nearest(yfit, visc_cut)
+
+        data['tg/tstar'].append(numerator/xfit[idx])
+        data['visc'].append(yfit[idx])
+
+        fragility_plots(
+                        fig,
+                        ax,
+                        numerator/x,
+                        y,
+                        numerator/xfit,
+                        yfit,
+                        visc_cut,
+                        i,
+                        )
+
+    ax.axhline(visc_cut, linestyle=':', label='Viscosity Choice')
+
+    fig.savefig('../correlation/fragility.png')
+    pl.close('all')
+
+    data = pd.DataFrame(data)
+    data.to_csv('../correlation/data.txt', index=False)
+    print(data)
+
+
+run_iterator(args.d, args.n, args.v,  args.t, ast.literal_eval(args.c))
--- tg.py
+++ tg.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as pl
-from functions import finder, data_parse
+
 from scipy.constants import physical_constants
 from scipy.interpolate import UnivariateSpline
+from functions import finder, data_parse
 from scipy.stats import linregress
+from knee import opt
 
 import pandas as pd
 import scipy as sc
@@ -37,207 +39,172 @@ parser.add_argument(
                     help='The file containing the hold temperature.'
                     )
 
+parser.add_argument(
+                    '-k',
+                    action='store',
+                    type=str,
+                    help='Spline fitting paramter for degree of smoothing spline.'
+                    )
+
+parser.add_argument(
+                    '-s',
+                    action='store',
+                    type=str,
+                    help='Spline fitting parameter for smoothing factor.'
+                    )
+
+
+parser.add_argument(
+                    '-a',
+                    action='store',
+                    type=str,
+                    help='The location to save data.'
+                    )
+
+
 args = parser.parse_args()
 
 
-def rmse(act, pred):
+def tg_plots(
+             x,
+             y,
+             k,
+             s,
+             ):
     '''
-    Calculate the root mean squared error (RMSE).
-
-    inputs:
-        act = The actual values.
-        pred = The predicted values.
-    outputs:
-        e = The RMSE.
-    '''
-
-    act = np.array(act)
-    pred = np.array(pred)
-
-    e = (pred-act)**2
-    e = e.mean()
-    e = np.sqrt(e)
-
-    return e
-
-
-def opt(x, y):
-    '''
-    Linearely fit data from both ends of supplied data and calculate RMSE.
-
-    Left fit:
-        Start at a pivot (x[0]) and then fit multiple lines from x[n] until
-        x[1]. Calculate RMSE for each fit.
-
-    Right fit:
-        Start at a pivot (x[n]) and then fit multiple lines from x[0] until
-        x[n-1]. Calculate RMSE for each fit.
-
-    After the fits are completed, their errors are averaged for each
-    x point included in a fit excluding the pivots.
-
-    The minimum averaged RMSE is considered to be the optimal point for
-    fitting two lines.
-
-    inputs:
-        x = Horizontal axis data.
-        y = Vertical axis data.
-    outputs:
-        xcut = The chosen x point.
-        endpoints = The non-pivot x values.
-        middlermse = The average RMSE.
-    '''
-
-    # Make data into numpy arrays
-    x = np.array(x)
-    y = np.array(y)
-
-    n = len(x)  # The length of data
-
-    ldata = []  # Left fits non-pivot and RMSE
-    rdata = []  # Right fits non-pivot and RMSE
-
-    for i in list(range(n-1)):
-
-        # Left fit
-        xl = x[:n-i]
-        yl = y[:n-i]
-
-        ml, il, _, _, _ = linregress(xl, yl)
-        yfitl = ml*xl+il
-        rmsel = rmse(yl, yfitl)  # Left RMSE
-
-        # Right fit
-        xr = x[i:]
-        yr = y[i:]
-
-        mr, ir, _, _, _ = linregress(xr, yr)
-        yfitr = mr*xr+ir
-        rmser = rmse(yr, yfitr)  # Right RMSE
-
-        # Exclude pivot points from data
-        if i > 0:
-            ldata.append((xl[-1], rmsel))
-            rdata.append((xr[0], rmser))
-
-    # Align data based on pivot point
-    ldata = np.array(ldata)
-    rdata = np.array(rdata[::-1])
-
-    middle_rmse = (ldata[:, 1]+rdata[:, 1])/2  # Mean RMSE
-    mcut = np.argmin(middle_rmse)  # Minimum RMSE index
-    xcut = ldata[mcut, 0]  # x data with minimum RMSE
-
-    endpoints = ldata[:, 0]  # Non-pivot points for fits
-
-    return xcut, endpoints, middle_rmse
-
-
-def plots(
-          x,
-          y,
-          xcut,
-          ycut,
-          xfitcut,
-          yfitcut,
-          k,
-          s,
-          tg,
-          endpoints,
-          middle_rmse
-          ):
-    '''
-    The plotting code for determining Tg.
+    The plotting code for Tg.
 
     inputs:
         x = The temperature data.
         y = The E-3kT data.
-        xcut = The cut temperature data.
-        ycut = The cut E-3kT data.
-        xcut = The cut temperature data fit with a spline.
-        ycut = The cut E-3kT data fit with a spline.
-        k = The degree of spline.
-        s = The spline smoothing factor.
-        tg = The glass transition temperature.
-        endpoints = The endpoints for error algorithm.
-        middle_rmse = The RMSE for error algorithm.
+        k = Spline fitting parameter.
+        s = Spline fitting parameter.
 
     outputs:
-        NAplots(x, y, k, s, tg, endpoints, middle_rmse)
+        tg = The glass transition temperature.
     '''
 
-    fig, ax = pl.subplots(2)
+    minx = x[x.argsort()[:6][-1]]  # Minimum number of points for spline to work
+    maxx = max(x)
 
-    ax[0].plot(
-               x,
-               y,
-               marker='.',
-               linestyle='none',
-               color='b',
-               label='data'
-               )
+    rangex = np.linspace(minx, maxx, 100)
 
-    ax[0].set_ylabel('E-3kT [K/atom]')
-    ax[0].grid()
-    ax[0].legend()
+    tgs = []
+    for i in rangex:
+        index = (x <= i)
+        xcut = x[index]
+        ycut = y[index]
 
-    ax[1].plot(
-               xcut,
-               ycut,
-               marker='.',
-               linestyle='none',
-               color='b',
-               label='data'
-               )
+        spl = UnivariateSpline(x=xcut, y=ycut, k=k, s=s)
+        xfitcut = np.linspace(min(xcut), max(xcut), 100)
+        yfitcut = spl(xfitcut)
 
-    ax[1].plot(
-               xfitcut,
-               yfitcut,
-               linestyle='-',
-               label='Univariate Spline (k='+str(k)+', s='+str(s)+')'
-               )
+        tg, endpoints, middle_rmse = opt(xfitcut, yfitcut)
 
-    ax[1].axvline(
-                  tg,
-                  linestyle='--',
-                  color='g',
-                  label='Tg = '+str(tg)+' [K]'
-                  )
+        tgs.append(tg)
 
-    ax[1].grid()
-    ax[1].legend()
+    tgs = np.array(tgs)
 
-    ax[1].set_xlabel('Temperature [K]')
-    ax[1].set_ylabel('E-3kT [K/atom]')
-
-    fig.set_size_inches(15, 10, forward=True)
-    pl.show()
-
-    pl.close('all')
+    index = np.argmax(tgs)
+    tg = tgs[index]
+    xcut = rangex[index]
 
     fig, ax = pl.subplots()
 
-    ax.plot(endpoints, middle_rmse, label='Mean RMSE')
+    ax.plot(rangex, tgs, marker='.', linestyle='none', label='Data')
+
+    ax.set_xlabel('Upper Temperature Cutoff [K]')
+    ax.set_ylabel('Tg [K]')
+
+    ax.grid()
+
+    fig.tight_layout()
+
+    index = (x <= xcut)
+    xnew = x[index]
+    ynew = y[index]
+
+    spl = UnivariateSpline(x=xnew, y=ynew, k=k, s=s)
+    xfitcut = np.linspace(xnew[0], xnew[-1], 100)
+    yfitcut = spl(xfitcut)
+
+    tg, endpoints, middle_rmse = opt(xfitcut, yfitcut)
+
+    ax.axvline(
+               xcut,
+               linestyle=':',
+               color='k',
+               label='Upper Temperature Cutoff: '+str(xcut)+' [K]'
+               )
+
+    ax.legend(loc='best')
+
+    fig, ax = pl.subplots()
+
+    ax.plot(
+            xnew,
+            ynew,
+            marker='.',
+            linestyle='none',
+            color='b',
+            label='data'
+            )
+
+    ax.plot(
+            xfitcut,
+            yfitcut,
+            linestyle=':',
+            color='k',
+            label='Spline Fit: (k='+str(k)+', s='+str(s)+')'
+            )
 
     ax.axvline(
                tg,
                linestyle='--',
-               color='r',
+               color='g',
                label='Tg = '+str(tg)+' [K]'
                )
 
-    ax.set_xlabel('End Temperature [K]')
-    ax.set_ylabel('RMSE')
     ax.grid()
     ax.legend()
 
+    ax.set_xlabel('Temperature [K]')
+    ax.set_ylabel('E-3kT [K/atom]')
+
     fig.tight_layout()
 
-    pl.show()
+    fig, ax = pl.subplots()
+
+    ax.plot(
+            x,
+            y,
+            marker='.',
+            linestyle='none',
+            color='b',
+            label='data'
+            )
+
+    ax.axvline(
+               tg,
+               linestyle='--',
+               color='g',
+               label='Tg = '+str(tg)+' [K]'
+               )
+
+    ax.grid()
+    ax.legend()
+
+    ax.set_xlabel('Temperature [K]')
+    ax.set_ylabel('E-3kT [K/atom]')
+
+    fig.tight_layout()
+
     pl.close('all')
 
+    return tg
 
-def run_iterator(path, filename, tempfile):
+
+def run_iterator(path, filename, tempfile, k, s):
     '''
     Iterate through each run and gather data.
 
@@ -245,6 +212,8 @@ def run_iterator(path, filename, tempfile):
         path = The path where run data is.
         filename = The name of the file containing thermodynamic data.
         tempfile = The name of the file containing the hold temperature.
+        k = Spline fitting parameter
+        s = Spline fitting parameter
 
     outputs:
         df = The data from runs.
@@ -254,7 +223,7 @@ def run_iterator(path, filename, tempfile):
     print(filename, path)
     paths = finder(filename, path)
 
-    k = physical_constants['Boltzmann constant in eV/K'][0]
+    kboltz = physical_constants['Boltzmann constant in eV/K'][0]
 
     # Gather all data and make one dataframe
     df = []
@@ -278,10 +247,10 @@ def run_iterator(path, filename, tempfile):
         df.append(data)
 
     df = pd.concat(df)
-    df['system'] = df['run'].apply(lambda x: x.split('/run/')[0])
+    df['job'] = df['run'].apply(lambda x: x.split('/run/')[0])
 
     # The number of atoms from naming convention
-    df['atoms'] = df['system'].apply(lambda x: re.split('(\d+)', x))
+    df['atoms'] = df['job'].apply(lambda x: re.split('(\d+)', x))
     df['atoms'] = df['atoms'].apply(lambda x: [i for i in x if i.isnumeric()])
     df['atoms'] = df['atoms'].apply(lambda x: [int(i) for i in x])
     df['atoms'] = df['atoms'].apply(lambda x: sum(x))
@@ -289,52 +258,38 @@ def run_iterator(path, filename, tempfile):
     # Calculate E-3kT
     df['etot'] = df['pe']+df['ke']
     df['etot/atoms'] = df['etot']/df['atoms']
-    df['etot/atoms-3kT'] = df['etot/atoms']-3.0*k*df['hold_temp']
+    df['etot/atoms-3kT'] = df['etot/atoms']-3.0*kboltz*df['hold_temp']
 
-    df['vol/atoms'] = df['vol']/df['atoms']
-
-    # Sort by temperature values
-    df = df.sort_values(by=['temp'])
-
-    groups = df.groupby(['system', 'hold_temp'])
+    # Group data by run
+    groups = df.groupby(['job', 'hold_temp'])
     mean = groups.mean().add_suffix('_mean').reset_index()
-    groups = mean.groupby(['system'])
+    groups = mean.groupby(['job'])
 
-    for i, j in groups: 
+    runs = []
+    tgs = []
+    for i, j in groups:
 
+        print(i)
         x = j['temp_mean'].values
         y = j['etot/atoms-3kT_mean'].values
 
-        # Filter repeated values
-        x, index = np.unique(x, return_index=True)
-        y = y[index]
+        tg = tg_plots(
+                      x,
+                      y,
+                      k,
+                      s,
+                      )
 
-        # Cutoff region
-        xcut = x
-        ycut = y
-
-        # Spline fit of cut region
-        k, s = (5, 1)
-        spl = UnivariateSpline(x=xcut, y=ycut, k=k, s=s)
-        xfitcut = np.linspace(xcut[0], xcut[-1], 100)
-        yfitcut = spl(xfitcut)
-
-        tg, endpoints, middle_rmse = opt(xfitcut, yfitcut)
-
-        print(i)
-        plots(
-              x,
-              y,
-              xcut,
-              ycut,
-              xfitcut,
-              yfitcut,
-              k,
-              s,
-              tg,
-              endpoints,
-              middle_rmse
-              )
+        runs.append(i)
+        tgs.append(tg)
 
 
-run_iterator(args.d, args.n, args.t)
+    df = pd.DataFrame({'job': runs, 'tg': tgs})
+
+    return df
+
+
+df = run_iterator(args.d, args.n, args.t, args.k, args.s)
+
+df.to_csv(os.path.join(args.a, 'tg.txt'), index=False)
+
